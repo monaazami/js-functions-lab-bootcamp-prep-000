f871df8ef6b0f2363fadc75f57ef362909824195
Mona Azami <mona.azami@gmail.com>
2018-03-22 15:03:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday,name){
-  return `Happy ${holiday}, ${name}`
+  return `Happy ${holiday}, ${name}!`
 }
 function holidaycountdown(holiday, days){
   return `It's ${days} days until ${holiday}!`
